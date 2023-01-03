7f16fefd0a492be68014a89c10b2bbc4561b212e
Gustavo Barbosa Scipiao Rodrigues <re045429@qintess.com>
2023-01-03 13:22:34 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/br/com/qintess/config/DevConfig.java
@@ -0,0 +1,31 @@
+package br.com.qintess.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.qintess.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	
+	@Autowired
+	private DBService service;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	
+	@Bean
+	public boolean initDB() {
+		if(value.equals("create")) {
+			this.service.initDB();
+		}
+		return false;
+	}
+	
+}
